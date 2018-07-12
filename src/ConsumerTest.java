eb2101fdcb956e624c76cb8918ca4dbf6baf0fdc
lenovo <291550492@qq.com>
2018-07-12 21:29:26 +0800

    Consumer 接口表示执行在单个参数上的操作。

---
+++ src/ConsumerTest.java
@@ -0,0 +1,8 @@
+import java.util.function.Consumer;
+
+public class ConsumerTest {
+    public static void main(String[] args) {
+        Consumer<Person> greeter = (p) -> System.out.println("Hello, " + p.firstName);
+        greeter.accept(new Person("Luke", "Skywalker"));
+    }
+}
