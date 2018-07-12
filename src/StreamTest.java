8a4395551c4b6b954441f0e276dba83e675d7072
lenovo <291550492@qq.com>
2018-07-12 22:20:16 +0800

    Count 计数   Match 匹配   Map 映射

--- src/StreamTest.java
+++ src/StreamTest.java
@@ -35,5 +35,34 @@ public class StreamTest {
                 .sorted((a, b) -> b.compareTo(a))   // a与b的位置换了，说明是从小到大排序
                 .forEach(System.out::println);
 // "DDD2", "DDD1", "CCC", "BBB3", "BBB2", "AAA2", "AAA1"
+
+        // Stream提供了多种匹配操作，允许检测指定的Predicate是否匹配整个Stream。所有的匹配操作都是最终操作，并返回一个boolean类型的值。
+        boolean anyStartsWithA =
+                stringCollection
+                        .stream()
+                        .anyMatch((s) -> s.startsWith("a"));
+        System.out.println(anyStartsWithA);      // true
+
+        boolean allStartsWithA =
+                stringCollection
+                        .stream()
+                        .allMatch((s) -> s.startsWith("a"));
+
+        System.out.println(allStartsWithA);      // false
+
+        boolean noneStartsWithZ =
+                stringCollection
+                        .stream()
+                        .noneMatch((s) -> s.startsWith("z"));
+
+        System.out.println(noneStartsWithZ);      // true
+
+        // 计数是一个最终操作，返回Stream中元素的个数，返回值类型是long。
+        long startsWithB =
+                stringCollection
+                        .stream()
+                        .filter((s) -> s.startsWith("b"))
+                        .count();
+        System.out.println(startsWithB);    // 3
     }
 }
