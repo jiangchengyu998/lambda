e1ca64a494c7a0753066d3053cd0d34f3955c235
lenovo <291550492@qq.com>
2018-07-12 21:22:15 +0800

    Function 接口有一个参数并且返回一个结果，并附带了一些可以和其他函数组合的默认方法（compose, andThen）

---
+++ src/FunctionTest.java
@@ -0,0 +1,10 @@
+import java.util.function.Function;
+
+public class FunctionTest {
+
+    public static void main(String[] args) {
+        Function<String, Integer> toInteger = Integer::valueOf;
+        Function<String, String> backToString = toInteger.andThen(String::valueOf);
+        System.out.println(backToString.apply("123"));
+    }
+}
